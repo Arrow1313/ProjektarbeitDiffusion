f5baa3da60b935f5470073336cb3ab421941b18d
Arrow1313 <henrik_jue@live.de>
2014-07-01 22:16:30 +0200

    Klasse Teilchen erstellt, implementierung fehlt noch

---
+++ Source/Teilchen.h
@@ -0,0 +1,17 @@
+/*
+ * Teilchen.h
+ *
+ *  Created on: 01.07.2014
+ *      Author: arrow13
+ */
+
+#ifndef TEILCHEN_H_
+#define TEILCHEN_H_
+
+class Teilchen {
+public:
+	Teilchen();
+	virtual ~Teilchen();
+};
+
+#endif /* TEILCHEN_H_ */
