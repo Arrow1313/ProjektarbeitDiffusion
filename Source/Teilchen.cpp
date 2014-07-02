370ad4c669ade7db39811ed289a981a3071e7e89
Arrow1313 <henrik_jue@live.de>
2014-07-02 21:08:35 +0200

    Teilchen Klasse bearbeitet

--- Source/Teilchen.cpp
+++ Source/Teilchen.cpp
@@ -1,17 +1,146 @@
 /*
  * Teilchen.cpp
  *
- *  Created on: 01.07.2014
+ *  Created on: 19.06.2014
+ *      Author: frederik
  */
 
 #include "Teilchen.h"
+#include "iostream"
+#include "math.h"
 
-Teilchen::Teilchen() {
-	// TODO Auto-generated constructor stub
+double Teilchen::dt = 0.1;
+
+Teilchen::Teilchen()
+:pos_x(0), pos_y(0), v_x(1), v_y(1), radius(1), masse(1), used(0)
+{
 
 }
 
-Teilchen::~Teilchen() {
-	// TODO Auto-generated destructor stub
+Teilchen::Teilchen(double x, double y, double x_v, double y_v, double r, double m)
+:used(0)
+{
+	this->pos_x = x;
+	this->pos_y = y;
+	this->v_x = x_v;
+	this->v_y = y_v;
+	this->radius = r;
+	this->masse = m;
 }
 
+
+void Teilchen::output_pos(){
+	std::cout << "pos_x: " << this->get_pos_x() << " pos_y: " << this->get_pos_y() << std::endl;
+}
+
+void Teilchen::output_v(){
+	std::cout << "v_x: " << this->get_v_x() << " pos_y: " << this->get_v_y() << std::endl;
+}
+
+double Teilchen::next_x_pos(){
+	return (this->pos_x + this->v_x * this->dt);
+}
+
+double Teilchen::next_y_pos(){
+	return (this->pos_y + this->v_y * this->dt);
+}
+
+void Teilchen::Kasten_Kollision(Kasten k){
+
+	//std::cout << "pos_x: " << this->pos_x << "\tnext_x: " << this->next_x_pos() << std::endl;
+	if(this->pos_x > k.x_rechts && this->next_x_pos() <= k.x_rechts){
+		this->v_x *= -1;
+		this->used = 1;
+		std::cout << "kollison mit der Wand rechts" << std::endl;
+	}
+
+	if(this->pos_x < k.x_links && this->next_x_pos() > k.x_links){
+		this->v_x *= -1;
+		this->used = 1;
+		std::cout << "kollison mit der Wand links" << std::endl;
+	}
+
+	if(this->pos_y < k.y_oben && this->next_y_pos() > k.y_oben){
+		this->v_y *= -1;
+		this->used = 1;
+		std::cout << "kollison mit der Wand oben" << std::endl;
+	}
+
+	if(this->pos_y > k.y_unten && this->next_y_pos() < k.y_unten){
+		this->v_y *= -1;
+		this->used = 1;
+		std::cout << "kollison mit der Wand unten" << std::endl;
+	}
+}
+
+void Teilchen::Teilchen_Kollision(Teilchen& t){
+	double x_abstand;
+	double y_abstand;
+	double abstand;
+	double minimal_abstand;
+	double temp_v_x_1;
+	double temp_v_y_1;
+	double temp_v_x_2;
+	double temp_v_y_2;
+
+	x_abstand = fabs(t.get_pos_x()- this->pos_x);
+	y_abstand = fabs(t.get_pos_y() - this->pos_y);
+	abstand = sqrt((pow(x_abstand,2.)+pow(y_abstand,2.)));
+	minimal_abstand = this->radius + t.get_radius();
+
+	if(abstand < minimal_abstand){
+		temp_v_x_1 = (this->masse*this->v_x + t.get_masse()*(2*t.get_v_x()-this->v_x))/(this->masse+t.get_masse());
+		temp_v_y_1 = (this->masse * this->v_y + t.get_masse()*(2*t.get_v_y()-this->v_y))/(this->masse+t.get_masse());
+
+		temp_v_x_2 = (t.get_masse()*t.get_v_x()+this->masse*(2*this->v_x-t.get_v_x()))/(this->masse+t.get_masse());
+		temp_v_y_2 = (t.get_masse()*t.get_v_y()+this->masse*(2*this->v_y-t.get_v_y()))/(this->masse+t.get_masse());
+
+		this->v_x = temp_v_x_1;
+		this->v_y = temp_v_y_1;
+
+		t.set_v_x(temp_v_x_2);
+		t.set_v_y(temp_v_y_2);
+
+		this->used = 1;
+		t.set_used(1);
+
+		std::cout << "Teilchen Kollison" << std::endl;
+	}
+
+}
+
+bool Teilchen::Teilchen_Kollision_b(Teilchen& t){
+	double x_abstand;
+		double y_abstand;
+		double abstand;
+		double minimal_abstand;
+
+		x_abstand = fabs(t.get_pos_x()- this->pos_x);
+		y_abstand = fabs(t.get_pos_y() - this->pos_y);
+		abstand = sqrt((pow(x_abstand,2.)+pow(y_abstand,2.)));
+		minimal_abstand = this->radius + t.get_radius();
+
+		if(abstand < minimal_abstand){
+			return 1;
+		}
+
+		return 0;
+}
+
+void Teilchen::Spalt_Kollision(Kasten k){
+	if((this->pos_x < 0 && this->next_x_pos() > 0)
+		|| (this->pos_x > 0 && this->next_x_pos() < 0)){
+		if(fabs(this->pos_y) > (k.get_size_spalt()/2)){
+			std::cout << "Kollsion Spalt" << std::endl;
+			this->v_x *= -1;
+			this->used = 1;
+		}
+	}
+}
+
+void Teilchen::Bewege(){
+	this->pos_x += this->dt * this->v_x;
+	this->pos_y += this->dt * this->v_y;
+
+	this->used = 0;
+}
--- Source/Teilchen.h
+++ Source/Teilchen.h
@@ -1,16 +1,62 @@
 /*
  * Teilchen.h
  *
- *  Created on: 01.07.2014
+ *  Created on: 19.06.2014
+ *      Author: frederik
  */
 
 #ifndef TEILCHEN_H_
 #define TEILCHEN_H_
 
+#include "Kasten.h"
+
 class Teilchen {
+private:
+	double pos_x; 	/** <Position des Teilchens in der X-Ebene.> */
+	double pos_y; 	/** <Position des Teilchens in der Y-Ebene.> */
+	double v_x; 	/** <Geschwindigkeit des Teilchens in X-Richtung.> */
+	double v_y; 	/** <Geschwindigkeit des Teilchens in Y-Richtung.> */
+	double radius;	/** <Radius des Teilches.> */
+	double masse;	/** <Masse des Teilchens.> */
+	bool used;
 public:
+
+	static double dt;
+	//getter-Funktion
+	/** \brief Gibt an, ob das Teilchen schon eine Kollison hatte oder nicht
+	*
+	*	Dieser Wert stellt sicher, dass Teilchen nicht durch eine Wand gedrückt werden.
+	*	Er wird in der Implementierung nur von derm
+	*/
+	bool get_used(){return used;};
+	double get_pos_x(){return pos_x;};
+	double get_pos_y(){return pos_y;};
+	double get_v_x(){return v_x;};
+	double get_v_y(){return v_y;};
+	double get_radius(){return radius;};
+	double get_masse(){return masse;};
+	//setter-Funktionen
+	void set_used(bool u){used = u;};
+	void set_pos_x(double x){pos_x = x;}
+	void set_pos_y(double y){pos_y = y;}
+	void set_v_x(double x){v_x = x;}
+	void set_v_y(double y){v_y = y;}
+	void set_radius(double r){radius = r;};
+	void set_masse(double m){masse = m;};
+
+	void output_pos();
+	void output_v();
+
 	Teilchen();
-	virtual ~Teilchen();
+	Teilchen(double,double,double,double,double,double);
+
+	void Kasten_Kollision(Kasten);
+	void Teilchen_Kollision(Teilchen&);
+	bool Teilchen_Kollision_b(Teilchen&);
+	void Spalt_Kollision(Kasten);
+	double next_x_pos();
+	double next_y_pos();
+	void Bewege();
 };
 
 #endif /* TEILCHEN_H_ */
