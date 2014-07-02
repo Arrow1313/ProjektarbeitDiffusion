296c3c40233e545e358251603955c4e891aaf05f
Arrow1313 <henrik_jue@live.de>
2014-07-03 01:32:49 +0200

    Grafikoutput funktioniert nun

--- Source/Grafikoutput.h
+++ Source/Grafikoutput.h
@@ -19,12 +19,15 @@ reset
 set terminal gif
 set output "Diffusion.gif"
 stats "Diffusion.txt" nooutput
-set pointsize 3
 unset xtic
 unset ytic
+set samples 1000
 set xrange [-Kasten.get_size_x()/2:Kasten.get_size_x()/2]
+set yrange [-Kasten.get_size_y()/2:Kasten.get_size_y()/2]
 do for [i=1:int(STATS_blocks)] {
-    plot 'Diffusion.txt'
+set arrow from 0,Spaltgröße/2 to 0,Kasten.get_size_y()/2 nohead
+set arrow from 0,-Spaltgröße/2 to 0,-Kasten.get_size_y()/2 nohead
+    plot 'Diffusion.txt' using 1:2 index i-1 with points pointsize "Teilchen.radius oder Default" pointtype 19
 }
 set output
 */
