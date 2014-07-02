e8e32575d387cc5673116a75fdd4f2a482ca7e08
Arrow1313 <henrik_jue@live.de>
2014-07-02 18:53:44 +0200

    Grafikoutput angefangen

---
+++ Source/Grafikoutput.h
@@ -0,0 +1,28 @@
+/*
+ * Grafikoutput.h
+ *
+ *  Created on: 01.07.2014
+ */
+
+#include "iostream"
+#include "stdlib.h"
+#include "stdio.h"
+#include "time.h"
+#include "Teilchen.h"
+#include "Kasten.h"
+
+using namespace std;
+
+//gnuplot
+//reset
+//set term gif animate
+//set output "Diffusion.gif"
+//n=    #n frames
+//set xrange [-Kastenbreite/2:Kastenbreite/2]
+//i=0
+//load "animate.gnuplot"
+//set output
+
+
+
+
