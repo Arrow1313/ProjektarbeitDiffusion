55617aad2e047b536cbf0cf44ec6d1101ab58922
Arrow1313 <henrik_jue@live.de>
2014-07-03 12:21:21 +0200

    Nun kann auch der Radius mitgeplottet werden

--- Source/Grafikoutput.h
+++ Source/Grafikoutput.h
@@ -16,18 +16,22 @@ using namespace std;
 /*
 gnuplot 
 reset
-set terminal gif animate delay "dt"
+set terminal gif animate delay "dt" size 1280,720
 set output "Diffusion.gif"
 stats "Diffusion.txt" nooutput
 unset xtic
 unset ytic
-set samples 1000
+unset key
+set samples 100
 set xrange [-Kasten.get_size_x()/2:Kasten.get_size_x()/2]
 set yrange [-Kasten.get_size_y()/2:Kasten.get_size_y()/2]
 do for [i=1:int(STATS_blocks)] {
 set arrow from 0,Spaltgröße/2 to 0,Kasten.get_size_y()/2 nohead
 set arrow from 0,-Spaltgröße/2 to 0,-Kasten.get_size_y()/2 nohead
-    plot 'Diffusion.txt' using 1:2 index i-1 with points pointsize "Teilchen.radius" pointtype 19
+#mit den arrows können wir die Spalte einstellen
+    plot 'Diffusion.txt' using 1:2:3 index i-1 with circles fs solid
+# Die dritte Spalte ist nun der Radius*Faktor, der von Gnuplot
+# abhängt, also können wir auch den Radius plotten
 }
 set output
 */
