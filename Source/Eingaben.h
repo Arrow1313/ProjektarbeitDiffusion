837a27ffcbba1eb87a1dc71191fc77decd245a38
Arrow1313 <henrik_jue@live.de>
2014-07-04 13:59:47 +0200

    Eingaben.h hinzugefügt

---
+++ Source/Eingaben.h
@@ -0,0 +1,18 @@
+
+
+#include "Kasten.h"
+#include "Teilchen.h"
+#include "fstream"
+#include "iostream"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include <sstream>
+
+using namespace std;
+
+#ifndef EINGABEN_H_
+#define EINGABEN_H_
+
+#endif /* EINGABEN_H_ */
+
