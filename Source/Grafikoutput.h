ca1107d2eda84725c6166098f300a626d46c2833
Arrow1313 <henrik_jue@live.de>
2014-07-06 20:51:21 +0200

    Erste Implementierung des Plots der Verteilung der Teilchen gegen die Zeit

--- Source/Grafikoutput.h
+++ Source/Grafikoutput.h
@@ -15,6 +15,14 @@ using namespace std;
 void Rahmendatei(Kasten);
 void Plotdaten(Teilchen [], int);
 void Plotradien(Teilchen [],int);
+void Plot_verteilung(Teilchen[],int);
+void Rahmendatei_verteilung();
+
+/**	\fn	Rahmendatei(Kasten k)
+ *
+ * 	Diese Funktion erstellt die Rahmendatei für die den Plot des Gifs und ruft das Script auf.
+ *
+ */
 
 void Rahmendatei(Kasten Kasten){
 
@@ -41,10 +49,18 @@ void Rahmendatei(Kasten Kasten){
 
 }
 
-void Plotdaten(Teilchen ar_t[],int anzahl){
+/**	\fn	Plotdaten(Teilchen ar_t[],int anzahl_teilchen)
+ *
+ * 	Diese Funktion speichert die Positionen und Radien in einer Datei, damit sie für den Plot der Gif verwendet werden können.
+ *
+ *	\param ar_t[]	Der Array in dem die Teilchen gespeicher sind.
+ *	\param	anzahl_teilchen	Die Anzahl der Teilchen in dem Array.
+ */
+
+void Plotdaten(Teilchen ar_t[],int anzahl_teilchen){
 	ofstream dat ("Diffusion.dat", ios::app);
 
-	for(int i = 0; i < anzahl; i++){
+	for(int i = 0; i < anzahl_teilchen; i++){
 		dat << ar_t[i].get_pos_x() << "\t" << ar_t[i].get_pos_y() << "\t" << ar_t[i].get_radius() << endl;
 	}
 
@@ -53,6 +69,47 @@ void Plotdaten(Teilchen ar_t[],int anzahl){
 	dat.close();
 }
 
+/**	\fn	Plot_verteilung(Teilchen ar_t[],int anzahl_teilchen)
+ *
+ * 	Diese Funktion liefer die Daten für den Plot der Teilchenverteilung in abhängigkeit der Zeit in einer Datei.
+ *
+ * 	\param ar_t[]	Der Array in dem die Teilchen gespeicher sind.
+ *	\param	anzahl_teilchen	Die Anzahl der Teilchen in dem Array.
+ */
+
+void Plot_verteilung(Teilchen ar_t[],int anzahl_teilchen){
+	ofstream dat ("Verlauf.dat", ios::app);
+
+	int rechts;
+	int links;
+
+	Teilchen_verteilung(ar_t, anzahl_teilchen, links, rechts);
+
+	for(int i = 0; i < anzahl_teilchen; i++){
+		dat << i <<  links << rechts << endl;
+	}
+
+	dat.close();
+}
+
+/**	\fn	Rahmendatei_verteilung()
+ *
+ * 	Die Funktion erstellt die Plotdatei, für die Verteilung der Teilchen in abhängigkeit er Zeit.
+ */
+
+void Rahmendatei_verteilung(){
+	ofstream rahmen ("rahmendatei_verteilung.plot");
+
+		rahmen << "reset" << endl
+			   << "set title \"Zeitliche Verteilung der Teilchen in den Kammern\""
+			   << "set terminal png size 1280,720" << endl
+			   << "set output \"Verteilung.png\"" << endl
+			   << "plot 'Verlauf.dat' using 1:2 title \"Anzahl der Teilchen auf der linke Seite\", 'Verlauf.dat' using 1:3 title \"Anzahl der Teilchen auf der linke Seite\" " << endl
+			   << "set output" << endl;
+
+		system("gnuplot rahmendatei.plot");
+
+}
 
 
 #endif /* GRAFIKOUTPUT_H_ */
