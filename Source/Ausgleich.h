635d7a7aac3b9f79921137fee8a5961bc74f23e8
Frederik S <frstrothmann@gmail.com>
2014-07-06 21:29:05 +0200

    Eingabe.h geschrieben und in Ausgleich.h noch eine Funktion hinzugefügt.

--- Source/Ausgleich.h
+++ Source/Ausgleich.h
@@ -1,13 +1,16 @@
-/*
- * Ausgleich.h
- *
- *  Created on: 30.06.2014
- */
-
 #ifndef AUSGLEICH_H_
 #define AUSGLEICH_H_
 
 bool Ausgeglichen(Teilchen [],int);
+void Teilchen_verteilung(Teilchen[],int,int&,int&);
+
+/**	\fn Ausgeglichen(Teilchen ar_t[], int anzahl)
+ * 	Diese Funktion gibt TRUE zurück, wenn sich gleich viele Teilchen links und rechts befinden.
+ * 	Bei ungerader Teilchenanzahl wird noch der Wert 1 addiert bzw. subtrahiert.
+ *
+ * 	\param	Teilchen ar_t[] Der Array in dem die Teilchen gespeichert sind.
+ * 	\param 	int anzahl Die Anzahl der Teilchen in dem Array.
+ */
 
 bool Ausgeglichen(Teilchen ar_t [], int anzahl){
 	int ungerade;
@@ -33,5 +36,28 @@ bool Ausgeglichen(Teilchen ar_t [], int anzahl){
 	return 0;
 }
 
+/**	\fn Teilchen_verteilung(Teilchen ar_t[], int anzahl_teilchen, int& links, int& rechts)
+ *
+ * 	Diese Funktion gibt in links und rechts die jeweilige Anzahl der Teilchen auf der entsprechenden Seite
+ *
+ * 	\param	ar_t[]	Der Array mit den Teilchen
+ * 	\param	anzahl_teilchen	Die Anzahl der Teilchen in dem Array
+ * 	\param	links	Die Variable, in der die Anzahl der Teilchen in der linken Kammer zurückgegeben wird.
+ * 	\param	rechts	Die Variable, in der die Anzahl der Teilchen in der rechten Kammer zurückgegeben wird.
+ *
+ */
+
+void Teilchen_verteilung(Teilchen ar_t[], int anzahl_teilchen, int& links, int& rechts){
+	int count_links = 0;
+
+	for(int i = 0; i < anzahl_teilchen; i++){
+			if(ar_t[i].get_pos_x() < 0){
+				count_links++;
+			}
+		}
+
+	links = count_links;
+	rechts = (anzahl_teilchen - count_links);
+}
 
 #endif /* AUSGLEICH_H_ */
--- Source/Eingaben.h
+++ Source/Eingaben.h
@@ -1,5 +1,3 @@
-
-
 #include "Kasten.h"
 #include "Teilchen.h"
 #include "fstream"
@@ -13,6 +11,89 @@ using namespace std;
 
 #ifndef EINGABEN_H_
 #define EINGABEN_H_
+
+void Eingabe_Kasten(Kasten&);
+int Eingabe_anzahl_teilchen();
+int Eingabe_anzahl_simulationen();
+bool Plotten();
+
+/**	\fn	Eingabe_Kasten()
+ *
+ * 	Dies Funktion liefert ein Interface, mit dem ein Kasten nach gewünschten Werten erstellt werden kann.
+ *
+ */
+
+Kasten Eingabe_Kasten(){
+	bool wahl;
+	cout << "Soll der standart Kasten(x=100,y=100,s=20) erstellt werden(0), oder wollen Sie selbst hand anlegen(1)?" << endl;
+	cin >> wahl;
+
+	if(wahl){
+		Kasten* temp_Kasten = new Kasten;
+
+		return *temp_Kasten;
+
+	} else {
+		double x_laenge;
+		double y_laenge;
+		double spalt;
+
+		cout << "Welche Länge in x-Richtung soll der Kasten haben?" << endl;
+		cin >> x_laenge;
+
+		cout << "Welche Länge in y-Richtung soll der Kastern haben?" << endl;
+		cin >> y_laenge;
+
+		cout << "Welche Breite soll der Spalt haben?" << endl;
+		cin >> spalt;
+
+		Kasten* temp_Kasten = new Kasten(x_laenge,y_laenge,spalt);
+
+		return *temp_Kasten;
+	}
+}
+
+/**	\fn	Eingabe_anzahl_teilchen()
+ *
+ * 	Mit dieser Funktion wird ein Interface geliefert, mit dem die Anzahl der Teilchen im Kasten eingegeben werden kann.
+ */
+
+int Eingabe_anzahl_teilchen(){
+	int anzahl_teilchen;
+	cout << "Wie viele Teilchen sollen erstellt werden?" << endl;
+	cin >> anzahl_teilchen;
+
+	return anzahl_teilchen;
+}
+
+/**	\fn	Eingabe_anzahl_simulationen()
+ *
+ * 	Die Funktion liefer ein Interface, mit dem eingegeben werden kann wie oft die Simulation wieder holt werden soll.
+ */
+
+int Eingabe_anzahl_simulationen(){
+	int anzahl_simulationen;
+	cout << "Wie oft soll die Simulation durchgeführt werden?" << endl;
+	cin >> anzahl_simulationen;
+
+	return anzahl_simulationen;
+}
+
+/**	\fn	Plotten()
+ *
+ * 	Die Funktion liefer ein Interface, für die Option ob der Durchlauf Graphisch dargestellt werden soll.
+ *
+ */
+
+bool Plotten(){
+	bool plotten;
+	cout << "Soll die nachfolgende Simulation Geplottet werden (1 = Ja, 0 = Nein)? Bitte beachten, dass Gnuplot 4.6 vorrausgesetzt wird." << endl;
+	cin >> plotten;
+	if(plotten){
+		cout << "Bitte passen Sie auf, dass der vorherige Plot überschrieben wird." << endl;
+	}
+	return plotten;
+}
 
 #endif /* EINGABEN_H_ */
 
